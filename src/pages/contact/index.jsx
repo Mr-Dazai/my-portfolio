64a7464f0eb2aa0b118860d8de36d92fe6f77156
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2023-08-13 12:58:38 -0400

    info

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -43,6 +43,7 @@ export const ContactUs = () => {
                 ''
               )}
             </address>
+            {/* <p>{contactConfig.description}</p> */}
           </Col>
         </Row>
       </Container>
